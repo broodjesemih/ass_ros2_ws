6320f262f67bc08288a64d3876fc2353ba74bcb0
Nout Mulder <noutmulder@quicknet.nl>
2025-10-07 17:21:40 +0200

    update tentamen_result_generator zodat hij ook aangestuurd kan worden door andere nodes

--- src/g1_ass1_pkg/src/tentamen_result_generator.cpp
+++ src/g1_ass1_pkg/src/tentamen_result_generator.cpp
@@ -7,7 +7,6 @@
 #include "g1_interface_pkg/msg/student.hpp"
 #include "database.cpp"
 
-
 struct StudentCourseKey
 {
     std::string student;
@@ -35,17 +34,20 @@ public:
     TentamenResultGenerator() : Node("tentamen_result_generator")
     {
         publisher_ = this->create_publisher<g1_interface_pkg::msg::Tentamen>("tentamen_results", 10);
+        student_control_sub_ = this->create_subscription<g1_interface_pkg::msg::Student>(
+            "student_control", 10,
+            std::bind(&TentamenResultGenerator::student_control_callback, this, std::placeholders::_1));
         timer_ = this->create_wall_timer(
             std::chrono::seconds(2),
             std::bind(&TentamenResultGenerator::publish_random_result, this));
         load_student_course_combinations();
     }
 
-    // Add/remove student/course combinations via service or topic (not implemented here for brevity)
 
 private:
     std::unordered_set<StudentCourseKey> active_combinations_;
     rclcpp::Publisher<g1_interface_pkg::msg::Tentamen>::SharedPtr publisher_;
+    rclcpp::Subscription<g1_interface_pkg::msg::Student>::SharedPtr student_control_sub_;
     rclcpp::TimerBase::SharedPtr timer_;
 
     void load_student_course_combinations()
@@ -79,6 +81,21 @@ private:
 
         publisher_->publish(msg);
         RCLCPP_INFO(this->get_logger(), "Published tentamen for %s/%s: %d", key.student.c_str(), key.course.c_str(), msg.tentamen_cijfer);
+    }
+
+    void student_control_callback(const g1_interface_pkg::msg::Student::SharedPtr msg)
+    {
+        StudentCourseKey key{msg->name, msg->course};
+        if (msg->command == "stop")
+        {
+            active_combinations_.erase(key);
+            RCLCPP_INFO(this->get_logger(), "Stopped generating for %s/%s", msg->name.c_str(), msg->course.c_str());
+        }
+        else if (msg->command == "again")
+        {
+            active_combinations_.insert(key);
+            RCLCPP_INFO(this->get_logger(), "Resumed generating for %s/%s", msg->name.c_str(), msg->course.c_str());
+        }
     }
 };
 
